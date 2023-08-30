7330e5aa8f9817bcc353787c4488aedecb9c6bd8
Emiliano Scavuzzo <emiacc@hotmail.com>
2023-08-30 13:20:20 -0300

    Fix build deps

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import inject from "@rollup/plugin-inject";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  define: {
+    "process.version": null
+  },
+  build: {
+    rollupOptions: {
+      plugins: [inject({ Buffer: ["Buffer", "Buffer"] })]
+    }
+  }
 });
